021afb78ef3790efd822517dc5fb9a11e1872f0e
inerpiece <anton.p.markov@gmail.com>
2024-05-26 23:30:07 +0300

    adds second test project

---
+++ src/app/projects/[project]/bgg2/page.tsx
@@ -0,0 +1,12 @@
+import FluidArticle from "@/components/FluidArticle";
+import FluidPage from "@/components/FluidPage";
+
+export default function Bgg2() {
+  return (
+    <FluidPage>
+      <FluidArticle>
+        <div>Working example 2...</div>
+      </FluidArticle>
+    </FluidPage>
+  );
+}
